e24604b76bfbaad97642bcddff6cd79779b2c76d
Nguyen Trung Hieu <hieu.nguyen5@monstar-lab.com>
2022-04-25 09:30:02 +0700

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import "./App.css";
-import Main from "./page/HomePage";
-
-import Footer from "./layout/Footer";
-import Header from "./layout/Header";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
     </div>
   );
 }
